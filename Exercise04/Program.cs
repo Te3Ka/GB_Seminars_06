e47e1115915b0b9a66c732a10db8124e40dddf7e
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-01 09:42:16 +0300

    Выполнено 4 и 5, начало 1

---
+++ Exercise04/Program.cs
@@ -0,0 +1,58 @@
+﻿/***********************/
+/*****Te3Ka_PaynE*******/
+/*Mnement4813@yandex.ru*/
+/***********************/
+
+/*
+Написать программу по выводу массива числел Фибоначчи для заданного элемента.
+Нельзя использовать рекурсию.
+*/
+
+//Метод с указанием автора
+void Author()
+{
+    Console.WriteLine();
+    Console.WriteLine("Программа создана Te3Ka_PaynE.");
+    Console.WriteLine("E-mail: Mnement4813@yandex.ru");
+    Console.WriteLine();
+}
+
+void PrintArray(uint[] arrayPruint)
+{
+    Console.Write("[");
+    for (int index = 0; index < arrayPruint.Length; index++)
+    {
+        if (index == (arrayPruint.Length - 1))
+            Console.WriteLine($"{arrayPruint[index]}]");
+        else
+            Console.Write($"{arrayPruint[index]}, ");
+    }
+}
+
+uint[] Fibonacci(uint number)
+{
+    uint[] arrayFib = new uint[number];
+    arrayFib[0] = 0;
+    arrayFib[1] = 1;
+    for(int i = 2; i < number; i++)
+    {
+        arrayFib[i] = arrayFib[i - 1] + arrayFib[i - 2];
+    }
+    return arrayFib;
+}
+
+uint GetVar()
+{  
+    Console.Write("Введите номер числа Фиббоначчи: ");
+    uint.TryParse(Console.ReadLine(), out uint num);
+    while (num <= 0)
+    {
+        Console.WriteLine("Число меньше 1, введите заново: ");
+        uint.TryParse(Console.ReadLine(), out num);
+    }
+    return num;
+}
+
+uint num = GetVar();
+Fibonacci(num);
+PrintArray(Fibonacci(num));
---
+++ Exercise05/Program.cs
@@ -0,0 +1,53 @@
+﻿int[] CreateRandomeArray(int num)
+{
+    int[] array = new int[num];
+    Random randome = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randome.Next(1, 100);
+    }
+    return array;
+}
+
+//Печать массива на экран
+void PrintArray(int[] arrayPrint)
+{
+    Console.Write("[");
+    for (int index = 0; index < arrayPrint.Length; index++)
+    {
+        if (index == (arrayPrint.Length - 1))
+            Console.WriteLine($"{arrayPrint[index]}]");
+        else
+            Console.Write($"{arrayPrint[index]}, ");
+    }
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] copyArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        copyArray[i] = array[i];
+    }
+    return copyArray;
+}
+
+int GetVar()
+{
+    Console.Write("Введите количество элементов массива: ");
+    int.TryParse(Console.ReadLine(), out int num);
+    while (num <= 0)
+    {
+        Console.WriteLine("Введено значение меньше 1, массив не может быть создан.\n"
+                        + "Повторите ввод количества элементов массива:");
+        int.TryParse(Console.ReadLine(), out num);
+    }
+    return num;
+}
+
+int num = GetVar();
+int[] array = CreateRandomeArray(num);
+Console.Write("Полученный массив:   ");
+PrintArray(array);
+Console.Write("Копированный массив: ");
+PrintArray(CopyArray(array));
